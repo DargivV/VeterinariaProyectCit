b65268269b953d2b25494248ae80e5233a5d39b6
DargivV <145886694+DargivV@users.noreply.github.com>
2024-10-07 14:30:18 -0500

    Primera practica beta1.1

--- src/main/java/com/mycompany/veterianradn/VeterianraDN.java
+++ src/main/java/com/mycompany/veterianradn/VeterianraDN.java
@@ -7,9 +7,17 @@ public class VeterianraDN {
         String [] HorarioDoc1 =  {"Lunes ","Miercoes ","jeuves ","Viernes"};
         
         Doctor doctor1 = new Doctor(01,"Ramirez","Gatos",HorarioDoc1,"5 pm ");
-       
-        
-        //acabas de unir el arrary con la creacion del obj doctor 
+       Cliente cliente1 = new Cliente (1,"roberto","Los nutriales", 94944);
+        Mascota mascota1 = new Mascota (1,"pimpon","Nutria", "Negra ",213,"12",cliente1 ) ;
+                
+        Mascota mascota2 = new Mascota (2,"Almorset","Cuy", "Mostasa ",213,"12",cliente1 ) ;
+         //public Citas(int ID_Cita, String Fecha, int hora, boolean Estado, Doctor doctor, Cliente paciente, Mascota mascota)
+         Citas cita1 = new Citas (1,"4/7/7",6,true ,doctor1,cliente1 ,mascota1);
+         
+         mascota1.AgregarCita(cita1);
+         System.out.print("- ");
+         mascota1.mostrarCitas();
+//acabas de unir el arrary con la creacion del obj doctor 
         
         //ahora crear el paciente y la mascota 
         // Crear un paciente
