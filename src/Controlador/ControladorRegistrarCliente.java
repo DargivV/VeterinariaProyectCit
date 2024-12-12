99b6effc965598453f5ef662b86eb087a7698280
DargivV <golden.clave@gmail.com>
2024-12-12 06:49:43 -0500

    PatitasSuaves_V_0.2.5 Avtualisacion BAsura

--- src/Controlador/ControladorRegistrarCliente.java
+++ src/Controlador/ControladorRegistrarCliente.java
@@ -21,30 +21,37 @@ public class ControladorRegistrarCliente implements ActionListener{
         data = ProcesosCliente.RecuperarDatosClientes();
         vista.btn_registrar.addActionListener(this);
         vista.btn_Actualisar.addActionListener(this);
-        vista.btn_buscar.addActionListener(this);
+   
         vista.btn_eliminar.addActionListener(this);
         ProcesosCliente.MostrarEnTabla(frmRC, data);
         ProcesosCliente.Presentacion(frmRC);
+        ActualizarForma();
         
         
-        
-    }
+    }void ActualizarForma() {
+    crud = new DAO_Cliente(); // Crear instancia del DAO
+    crud.MostrarCliente(vista.tbl_RCliente); // Llenar tabla y actualizar etiqueta
+ 
+}
+
     @Override
     public void actionPerformed(ActionEvent e) {
         
       if (e.getSource()==vista.btn_registrar){
       client = ProcesosCliente.LeerDatos(vista);
       crud = new DAO_Cliente();
-      crud.insertarCliente(client);
-      //me4todo pa mostrar tabas aqui    
+      crud.insertarCliente(client); 
+      ActualizarForma();
+       
       }  
       
          
       if (e.getSource()==vista.btn_Actualisar){
       client = ProcesosCliente.LeerDatos(vista);
       crud = new DAO_Cliente();
-      crud.actualizarCliente(client);
-      //me4todo pa mostrar tabas aqui    
+      crud.actualizarCliente(client); 
+      
+       ActualizarForma();  
       }  
       
      if (e.getSource() == vista.btn_eliminar) {
@@ -60,12 +67,16 @@ public class ControladorRegistrarCliente implements ActionListener{
             DAO_Cliente crud = new DAO_Cliente();
             crud.eliminarCliente(client.getDNI());
              JOptionPane.showMessageDialog(null, "Cliente eliminado correctamente.");
-            //   actualizar 
+            //   actualiza
+             ActualizarForma();
+            
                     }
     } catch (Exception ex) {
         JOptionPane.showMessageDialog(null, "ERROR: No se pudo eliminar el cliente. " + ex.getMessage());
     }
     ///////////////////////////////////////////////////////////
+ 
+
  
      
     
